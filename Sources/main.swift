9590854c54c40c06120fa8b664e4dda41bd90cf2
骆昱 (Mac mini) <indie.luo@gmail.com>
2017-09-01 17:26:13 +0800

    [Add] router

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,13 @@
 import Kitura
+
+let router = Router()
+
+router.get("/") {
+	request, response, next in
+	response.send("<html><body><h2>hello, love</h2></body></html>")
+	next()
+}
+
+Kitura.addHTTPServer(onPort: 8080, with: router)
+
+Kitura.run()
