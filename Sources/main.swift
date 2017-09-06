9f02d2add303d1f0d6128efb37f58dd53d83af22
Luo Yu <indie.luo@gmail.com>
2017-09-06 14:33:55 +0800

    [Modify] enable md template

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,6 +1,12 @@
 import Kitura
+import KituraMarkdown
+
+// MARK: - ROUTER
 
 let router = Router()
+
+// Add KituraMarkdown as a TemplatingEngine
+router.add(templateEngine: KituraMarkdown())
 
 router.get("/") {
 	request, response, next in
@@ -13,6 +19,18 @@ router.get("/name/:name") { request, response, _ in
 	try response.send("Hello \(name)").end()
 }
 
+
+// MARK: - DOCS
+
+// Handle HTTP GET requests to /docs
+router.get("/docs") { _, response, next in
+	try response.render("/docs/index.md", context: [String:Any]())
+	response.status(.OK)
+	next()
+}
+
+// MARK: - HTTP SERVER
 Kitura.addHTTPServer(onPort: 8080, with: router)
 
+// MARK: - RUN APP
 Kitura.run()
