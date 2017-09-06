0d083c115ecc7e50cdd2d3fdbaf380e3d9c18306
Luo Yu <indie.luo@gmail.com>
2017-09-06 14:41:13 +0800

    [Modify] package with template engine

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "app.luoyu.space",
     dependencies: [
-		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion:1, minor:7)
+		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion:1, minor:7),
+		.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 7)
 	]
 )
