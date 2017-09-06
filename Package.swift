6b1331995c7937c922f1ccaa4bd443733c54698d
Luo Yu <indie.luo@gmail.com>
2017-09-06 15:42:55 +0800

    [Modify] dependency markdown

--- Package.swift
+++ Package.swift
@@ -6,6 +6,6 @@ let package = Package(
     name: "app.luoyu.space",
     dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion:1, minor:7),
-		.Package(url: "https://github.com/IBM-Swift/Kitura-TemplateEngine.git", majorVersion: 1, minor: 7)
+		.Package(url: "https://github.com/IBM-Swift/Kitura-Markdown.git", majorVersion:0, minor:9)
 	]
 )
